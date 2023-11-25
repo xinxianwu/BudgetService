52fe0f774c8b4f70b7616ae4fdaf4275a2656164
Xinxian Wu <xinxian.wu@onelab.tw>
2023-11-25 16:12:36 +0800

    init project

---
+++ BudgetService/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BudgetService;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
