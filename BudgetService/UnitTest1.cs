93971476cd679e5a380ca94b7ce1a1c2b15cd4b1
Xinxian Wu <xinxian.wu@onelab.tw>
2023-11-25 16:29:41 +0800

    add Query_Monthly

--- BudgetService/UnitTest1.cs
+++ BudgetService/UnitTest1.cs
@@ -1,15 +1,67 @@
-namespace BudgetService;
+using FluentAssertions;
+using NSubstitute;
+
+namespace Trainning;
 
 public class Tests
 {
+    private IBudgetRepo? _budgetRepo;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _budgetRepo = Substitute.For<IBudgetRepo>();
+    }
+
     [SetUp]
     public void Setup()
     {
     }
 
     [Test]
-    public void Test1()
+    public void Query_Monthly()
     {
-        Assert.Pass();
+        _budgetRepo!.GetAll().Returns(new List<Budget>
+        {
+            new()
+            {
+                YearMonth = "202311",
+                Amount = 30
+            }
+        });
+
+        var budgetService = new BudgetService(_budgetRepo);
+
+        var budget = budgetService.Query(new DateTime(2023, 11, 1), new DateTime(2023, 11, 30));
+
+        budget.Should().Be(30);
+    }
+}
+
+public interface IBudgetRepo
+{
+    List<Budget> GetAll();
+}
+
+public class Budget
+{
+    public string YearMonth { get; set; } = string.Empty;
+    public int Amount { get; set; }
+}
+
+public class BudgetService
+{
+    private readonly IBudgetRepo _budgetRepo;
+
+    public BudgetService(IBudgetRepo budgetRepo)
+    {
+        _budgetRepo = budgetRepo;
+    }
+
+    public decimal Query(DateTime startTime, DateTime endTime)
+    {
+        var budgets = _budgetRepo.GetAll();
+
+        return budgets.Sum(budget => budget.Amount);
     }
 }
